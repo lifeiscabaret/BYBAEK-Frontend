2a705ba1536dabb38d0339133c2ae4e9e4bb5c00
TaekyoungLee02 <tkdr2002@gmail.com>
2026-03-05 11:55:15 +0900

    [add] standalone deploy

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  output: "standalone"
 };
+
+module.exports = { output: "standalone" };
 
 export default nextConfig;
