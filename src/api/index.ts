6a95d714a097e14ed013c91ae0a3414647122b9c
jihyun <lifeiscabaret1998@gmail.com>
2026-04-07 23:46:35 +0900

    [FIX] Add /api prefix to apiClient baseURL

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const apiClient = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_API_BASE_URL || "https://bybaek-b-bzhhgzh8d2gthpb3.koreacentral-01.azurewebsites.net",
+  baseURL: process.env.NEXT_PUBLIC_API_BASE_URL || "https://bybaek-b-bzhhgzh8d2gthpb3.koreacentral-01.azurewebsites.net/api",
   withCredentials: true,
 
   timeout: 60000,
