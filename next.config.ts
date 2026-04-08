68b5d5fa1fdbbddcedf031894a7144c34029d8af
jihyun <lifeiscabaret1998@gmail.com>
2026-04-08 21:30:08 +0900

    [CHORE] Add Azure Blob Storage domain to Next.js image config

--- next.config.ts
+++ next.config.ts
@@ -22,6 +22,12 @@ const nextConfig: NextConfig = {
         port: '',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'bybaekstorage.blob.core.windows.net',
+        port: '',
+        pathname: '/**',
+      },
     ],
   },
 };
